fcc178f6c0307e7a7031fe13e0ead11039d88ada
Sanchay13 <112164807+Sanchay13@users.noreply.github.com>
2022-12-20 20:45:12 +0100

    kommentarer

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-var svar = [0, 0, 0, 0, 0]
+var svar = [0, 0, 0, 0, 0];
 
 function livesvar(){
     var selecteduenig = false;
     var selectedenig = false;
     
-    for(let i = 1; i < svar.length; i++){
-        if(document.getElementById("uenig"+i).checked){
+    for(let i = 1; i < svar.length; i++){ // loop som kjører 4 ganger (fordi det er fire spørsmål)
+        if(document.getElementById("uenig"+i).checked){ // Hvis uenig er checked så skal valuen for dette displayes på skjermen.
             selecteduenig = true; 
             answer = document.getElementById("uenig"+i).value;
             document.getElementById("Svar"+i).innerHTML = answer;
         }
     
-        if(document.getElementById("enig"+i).checked){
+        if(document.getElementById("enig"+i).checked){ // Hvis enig er checked så skal valuen for dette displayes på skjermen.
             selectedenig = true;
             answer = document.getElementById("enig"+i).value;
             document.getElementById("Svar"+i).innerHTML = answer;
@@ -19,11 +19,11 @@ function livesvar(){
     }
 }
 
-function EndeligResultat(){
-    svar[0]++
+function EndeligResultat(){ // Når svar knappen trykkes skal følgende kode kjøre:
+    svar[0]++ // Vi trekker index 0 fra arrayen i svar variablen, dette bruker vi for å registrere antall besvarelser med svar[0]++.
     console.log("Antall besvarelser:" + svar[0]);
 
-    if(document.getElementById("enig1").checked){
+    if(document.getElementById("enig1").checked){ // På samme måte så trekker vi restværende index-ene fra arrayen. Vi kjører fire if-statement som sjekker om enig er checked, hvis det er det skal det plusses på med en.
         svar[1]++;
     }
     if(document.getElementById("enig2").checked){
@@ -38,12 +38,12 @@ function EndeligResultat(){
     console.log(svar);
 }
 
-function Result(){
-var xValues = ["Himmelen er grønn", "Jorden er flat", "Venus er firkantet", "Saturn er rund"];
-var yValues = [(svar[1]/svar[0])*100, (svar[2]/svar[0])*100, (svar[3]/svar[0])*100, (svar[4]/svar[0])*100];
-var barColors = ["red", "yellow","blue","orange","brown"];
+function Result(){ // Når vis resultat knappen trykkes på skal følgende kode kjøres: // Vi bruker chart.js som er importert i html. 
+var xValues = ["Himmelen er grønn", "Jorden er flat", "Venus er firkantet", "Saturn er rund"]; // Array med verdiene for tekst i søyle diagrammet
+var yValues = [(svar[1]/svar[0])*100, (svar[2]/svar[0])*100, (svar[3]/svar[0])*100, (svar[4]/svar[0])*100]; // bruker prosent regning for å få svaret prosentvis.
+var barColors = ["red", "yellow","blue","orange","brown"]; // Definerer fargen på bar-ene.
 
-new Chart("myChart", {
+new Chart("myChart", { // Kode som creater chart:
     type: "bar",
     data: {
     labels: xValues,
